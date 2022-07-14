44ef6f76eca46e30c47ba424c4b780362c1cfde7
João Paulo de Lima Novaes <94202128+Jplnovaes@users.noreply.github.com>
2022-07-14 11:55:53 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+var countDownDate = new Date("July 26, 2022 20:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24)); var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("dday").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if (distance < 0) { clearInterval(x); document.getElementById("dday").innerHTML = "SITE NA TEIA"; }
+}, 1000);
+
